53c2ebc3ed118272d118abec721743040f47655d
Travis Perdue <travis.perdue@rallyhealth.com>
2022-04-28 13:52:59 -0500

    temp dev logging

--- pkg/apis/rollouts/validation/validation_references.go
+++ pkg/apis/rollouts/validation/validation_references.go
@@ -251,7 +251,7 @@ func ValidateIngress(rollout *v1alpha1.Rollout, ingress *ingressutil.Ingress) fi
 
 func reportErrors(ingress *ingressutil.Ingress, serviceName, ingressName string, fldPath *field.Path, allErrs field.ErrorList) field.ErrorList {
 	if !ingressutil.HasRuleWithService(ingress, serviceName) {
-		msg := fmt.Sprintf("ingress `%s` has no rules using service %s backend", ingress.GetName(), serviceName)
+		allErrs = append(allErrs, field.Invalid(fldPath, ingressName, "ValidateIngress"))
 		allErrs = append(allErrs, field.Invalid(fldPath, ingressName, msg))
 	}
 	return allErrs
